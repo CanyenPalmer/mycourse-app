e78fe4ae558c2ea1c0642ab8f3a091c5f1767f3e
Canyen Palmer <Canyen2019@gmail.com>
2025-08-05 20:31:42 -0400

    Update models.py

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ db = SQLAlchemy()
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.String(128), nullable=False)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -21,9 +21,9 @@ class Round(db.Model):
     course_name = db.Column(db.String(120))
     tee_box = db.Column(db.String(50))
     date_played = db.Column(db.String(20))
-    strokes = db.Column(db.PickleType)  # list of strokes per hole
-    putts = db.Column(db.PickleType)    # list of putts per hole
-    pars = db.Column(db.PickleType)     # list of par values
+    strokes = db.Column(db.PickleType)
+    putts = db.Column(db.PickleType)
+    pars = db.Column(db.PickleType)
 
 class CourseTemplate(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -31,9 +31,3 @@ class CourseTemplate(db.Model):
     par_list = db.Column(db.Text)  # JSON-encoded list of pars
     par_total = db.Column(db.Integer)
     created_count = db.Column(db.Integer, default=1)
-
-class UserTemplate(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
-    course_template_id = db.Column(db.Integer, db.ForeignKey("course_template.id"))
-    custom_name = db.Column(db.String(256))
