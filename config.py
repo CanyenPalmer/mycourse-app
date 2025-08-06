1bac4083afa867c031775e023f37885de2fbce32
Canyen Palmer <Canyen2019@gmail.com>
2025-08-05 20:32:34 -0400

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,3 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "default-dev-key")
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///site.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
