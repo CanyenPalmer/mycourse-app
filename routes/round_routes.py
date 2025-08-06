65f62dc55aebb3eeb9c92f573ea4f98cb969eca8
Canyen Palmer <Canyen2019@gmail.com>
2025-08-05 21:21:32 -0400

    Update round_routes.py

--- routes/round_routes.py
+++ routes/round_routes.py
@@ -2,7 +2,7 @@
 
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from flask_login import login_required, current_user
-from models import db, Round, Course
+from models import db, Round, CourseTemplate
 
 round_bp = Blueprint('round', __name__)
 
