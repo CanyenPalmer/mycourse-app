9b8d479343c02328ce9ce6229a970426d51d380c
Canyen Palmer <Canyen2019@gmail.com>
2025-08-05 21:04:52 -0400

    Update models.py

--- models.py
+++ models.py
@@ -1,32 +1,43 @@
-# models.py
-
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
+from datetime import datetime
 
 db = SQLAlchemy()
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(150), nullable=False, unique=True)
-    password = db.Column(db.String(150), nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
     rounds = db.relationship('Round', backref='user', lazy=True)
-
-class Course(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), nullable=False)
-    par_values = db.Column(db.String(255), nullable=False)  # e.g., "4,4,3,5,4,..."
-    tee_box = db.Column(db.String(100))
-    total_par = db.Column(db.Integer)
-    holes_played = db.Column(db.Integer)
-    rounds = db.relationship('Round', backref='course', lazy=True)
 
 class Round(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.String(100), nullable=False)
-    score = db.Column(db.Integer, nullable=False)
-    fairways_hit = db.Column(db.Integer)
-    greens_in_regulation = db.Column(db.Integer)
-    putts = db.Column(db.Integer)
-    penalties = db.Column(db.Integer)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    course_id = db.Column(db.Integer, db.ForeignKey('course.id'), nullable=True)
+    course_name = db.Column(db.String(200), nullable=False)
+    tee_box = db.Column(db.String(100), nullable=False)
+    holes_played = db.Column(db.Integer, nullable=False)
+    starting_hole = db.Column(db.Integer, nullable=False)
+    total_score = db.Column(db.Integer, nullable=False)
+    date_played = db.Column(db.DateTime, default=datetime.utcnow)
+    notes = db.Column(db.Text, nullable=True)
+    stats = db.relationship('Stat', backref='round', lazy=True)
+
+class Stat(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    round_id = db.Column(db.Integer, db.ForeignKey('round.id'), nullable=False)
+    hole_number = db.Column(db.Integer, nullable=False)
+    par = db.Column(db.Integer, nullable=False)
+    strokes = db.Column(db.Integer, nullable=False)
+    fairway_hit = db.Column(db.Boolean, nullable=True)
+    green_in_regulation = db.Column(db.Boolean, nullable=True)
+    putts = db.Column(db.Integer, nullable=True)
+
+class CourseTemplate(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    par_values = db.Column(db.PickleType, nullable=False)
+    created_by = db.Column(db.String(120), nullable=True)
+
+    def __repr__(self):
+        return f'<CourseTemplate {self.name} | Par {sum(self.par_values)}>'
