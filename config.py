6cf29a2934c8f60785c66bafbd3224330df3d906
Canyen Palmer <Canyen2019@gmail.com>
2025-08-05 20:20:54 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,10 @@
+# config.py
+
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY", "default-secret-if-not-set")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///site.db")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    FLASK_ENV = os.environ.get("FLASK_ENV", "production")
 # Placeholder for config.py
