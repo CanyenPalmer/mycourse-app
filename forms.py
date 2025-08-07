cdc7a5406562a967fe288b7e7e958ac534f4b5d2
Canyen Palmer <Canyen2019@gmail.com>
2025-08-06 21:51:28 -0400

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, SelectField
 from wtforms.validators import DataRequired, Email
 
 class RegistrationForm(FlaskForm):
@@ -12,3 +12,6 @@ class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
+
+class StartRoundForm(FlaskForm):
+    course_name = StringField('Course Name', valid_
