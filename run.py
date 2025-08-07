0997f4b60b43466687dbb66f35667bb80054a04c
Canyen Palmer <Canyen2019@gmail.com>
2025-08-06 21:46:13 -0400

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,5 @@
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
-
+# run.py
 from app import app
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
