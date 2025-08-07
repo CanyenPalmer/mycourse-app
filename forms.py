e0a9ed1cd5c8d332240dc04a6280ab9f74aee217
Canyen Palmer <Canyen2019@gmail.com>
2025-08-06 21:35:35 -0400

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,23 +1,14 @@
-# forms.py
-
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, SelectField
-from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email
 
-class RegisterForm(FlaskForm):
-    username = StringField("Username", validators=[DataRequired()])
-    email = StringField("Email", validators=[DataRequired(), Email()])
-    password = PasswordField("Password", validators=[DataRequired()])
-    confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo("password")])
-    submit = SubmitField("Register")
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Register')
 
 class LoginForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired(), Email()])
-    password = PasswordField("Password", validators=[DataRequired()])
-    submit = SubmitField("Login")
-
-class StartRoundForm(FlaskForm):
-    course_name = StringField("Course Name", validators=[DataRequired()])
-    tee = SelectField("Tee", choices=[("Back", "Back"), ("Middle", "Middle"), ("Front", "Front")], validators=[DataRequired()])
-    holes = SelectField("Number of Holes", choices=[("9", "9"), ("18", "18")], validators=[DataRequired()])
-    submit = SubmitField("Start Round")
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
