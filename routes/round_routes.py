df91237cfeb863e6033e6b2a73a9ff0e99495bb5
Canyen Palmer <Canyen2019@gmail.com>
2025-08-06 21:21:46 -0400

    Update round_routes.py

--- routes/round_routes.py
+++ routes/round_routes.py
@@ -3,20 +3,19 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from flask_login import login_required, current_user
 from models import db, Round, CourseTemplate
+from forms import StartRoundForm
 
 round_bp = Blueprint('round', __name__)
 
 @round_bp.route('/start-round', methods=['GET', 'POST'])
 @login_required
 def start_round():
-    if request.method == 'POST':
-        course_name = request.form['course_name']
+    form = StartRoundForm()
+    if form.validate_on_submit():
         par_values = request.form.getlist('par_values[]')
-        tee = request.form['tee']
-        holes = int(request.form['holes'])
 
         new_course = CourseTemplate(
-            course_name=course_name,
+            course_name=form.course_name.data,
             hole_pars=",".join(par_values),
             total_par=sum(map(int, par_values))
         )
@@ -25,7 +24,7 @@ def start_round():
 
         new_round = Round(
             user_id=current_user.id,
-            course_name=course_name,
+            course_name=form.course_name.data,
             total_par=new_course.total_par,
             total_score=0,
             fairways_hit=0,
@@ -39,24 +38,7 @@ def start_round():
 
         return redirect(url_for('round.edit_round', round_id=new_round.id))
 
-    return render_template('start_round.html')
-
-@round_bp.route('/round/<int:round_id>/edit', methods=['GET', 'POST'])
-@login_required
-def edit_round(round_id):
-    round_data = Round.query.get_or_404(round_id)
-
-    if request.method == 'POST':
-        round_data.total_score = int(request.form.get('total_score', 0))
-        round_data.fairways_hit = int(request.form.get('fairways_hit', 0))
-        round_data.greens_in_reg = int(request.form.get('greens_in_reg', 0))
-        round_data.putts = int(request.form.get('putts', 0))
-        round_data.penalties = int(request.form.get('penalties', 0))
-        round_data.notes = request.form.get('notes', '')
-        db.session.commit()
-        return redirect(url_for('round.round_summary', round_id=round_data.id))
-
-    return render_template('edit_round.html', round=round_data)
+    return render_template('start_round.html', form=form)
 
 @round_bp.route('/round/<int:round_id>/summary')
 @login_required
@@ -64,9 +46,4 @@ def round_summary(round_id):
     round_data = Round.query.get_or_404(round_id)
     return render_template('round_summary.html', round=round_data)
 
-@round_bp.route('/past_rounds')
-@login_required
-def past_rounds():
-    user_rounds = Round.query.filter_by(user_id=current_user.id).order_by(Round.date_played.desc()).all()
-    return render_template('past_rounds.html', rounds=user_rounds)
-
+@round_bp.route('/_
