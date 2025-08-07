df5af820c9a3011c5118fcbcce4ffd78b32eca89
Canyen Palmer <Canyen2019@gmail.com>
2025-08-06 21:20:41 -0400

    Update forms.py

--- routes/forms.py
+++ routes/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, SelectField
 from wtforms.validators import DataRequired, Email, EqualTo
 
 class RegisterForm(FlaskForm):
