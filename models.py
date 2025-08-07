fc7eeb93c60d084c9828258141e9b7504807a999
Canyen Palmer <Canyen2019@gmail.com>
2025-08-06 21:42:08 -0400

    Update models.py

--- models.py
+++ models.py
@@ -1,6 +1,5 @@
-# models.py
-from extensions import db
 from flask_login import UserMixin
+from extensions import db
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -8,4 +7,15 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(150), nullable=False, unique=True)
     password = db.Column(db.String(256), nullable=False)
 
-# Other models (Round, CourseTemplate, etc.)
+class Round(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    course_name = db.Column(db.String(150))
+    date_played = db.Column(db.Date)
+    score = db.Column(db.Integer)
+    notes = db.Column(db.Text)
+
+class CourseTemplate(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    course_name = db.Column(db.String(150), nullable=False)
+    par_values = db.Column(db.String(100), nullable=False)  # comma-separated string of pars
