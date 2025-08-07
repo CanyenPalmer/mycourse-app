9b49765b30d42750977080c7f5d254522edf5a36
Canyen Palmer <Canyen2019@gmail.com>
2025-08-06 22:07:38 -0400

    Update dashboard_routes.py

--- routes/dashboard_routes.py
+++ routes/dashboard_routes.py
@@ -1,13 +1,4 @@
-from flask import Blueprint, render_template
-from flask_login import login_required, current_user
-from models import Round
-from io import BytesIO
-import base64
-import matplotlib.pyplot as plt
-from sqlalchemy import desc
-
-dashboard_bp = Blueprint('dashboard', __name__)
-
+@dashboard_bp.route('/')
 @dashboard_bp.route('/dashboard')
 @login_required
 def dashboard():
